62682b75983dac70b1b5c45ccc1e85c44fc501ff
long <hoangthanhlong13032000@gmail.com>
2021-05-24 15:42:25 +0700

    Change backend api successfully

--- uet_dic/lib/share/app_api.dart
+++ uet_dic/lib/share/app_api.dart
@@ -1,11 +1,11 @@
 
-final _backendIp = '192.168.2.103';
+// final _backendIp = '192.168.2.103';
 // final _backendIp = '192.168.1.114';
 // final _backendIp = '192.168.43.200';
 
-final _backendPort = 3333;
+// final _backendPort = 3333;
 
-final _api = 'http://$_backendIp:$_backendPort/api';
+final _api = 'http://api.bcchelloworld.design/api';
 
 final signInApi = '$_api/auth/signin';
 
